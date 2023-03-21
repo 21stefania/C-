14889297f95939260a10d1fc290ebeb4aece17f5
21stefania <111510153+21stefania@users.noreply.github.com>
2023-03-21 09:51:48 -0500

    Add files via upload

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,306 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ArchivoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EdicionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ContextMenuStrip2 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ArchivoToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ArchivoToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NurevoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ABRIRToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.GuardarComoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EdicionToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AtrasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AdelanteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CerrarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CopiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PegarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LimipiarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FormatoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FuenteToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ColorToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NegrillaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CurvaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SubrayadoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AcercaDeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CrefditosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ContextMenuStrip1.SuspendLayout()
+        Me.ContextMenuStrip2.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(116, 26)
+        '
+        'ArchivoToolStripMenuItem
+        '
+        Me.ArchivoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.EdicionToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem.Name = "ArchivoToolStripMenuItem"
+        Me.ArchivoToolStripMenuItem.Size = New System.Drawing.Size(115, 22)
+        Me.ArchivoToolStripMenuItem.Text = "Archivo"
+        '
+        'EdicionToolStripMenuItem
+        '
+        Me.EdicionToolStripMenuItem.Name = "EdicionToolStripMenuItem"
+        Me.EdicionToolStripMenuItem.Size = New System.Drawing.Size(113, 22)
+        Me.EdicionToolStripMenuItem.Text = "Edicion"
+        '
+        'ContextMenuStrip2
+        '
+        Me.ContextMenuStrip2.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem1})
+        Me.ContextMenuStrip2.Name = "ContextMenuStrip2"
+        Me.ContextMenuStrip2.Size = New System.Drawing.Size(116, 26)
+        '
+        'ArchivoToolStripMenuItem1
+        '
+        Me.ArchivoToolStripMenuItem1.Name = "ArchivoToolStripMenuItem1"
+        Me.ArchivoToolStripMenuItem1.Size = New System.Drawing.Size(115, 22)
+        Me.ArchivoToolStripMenuItem1.Text = "Archivo"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ArchivoToolStripMenuItem2, Me.EdicionToolStripMenuItem1, Me.FormatoToolStripMenuItem, Me.AyudaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(800, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ArchivoToolStripMenuItem2
+        '
+        Me.ArchivoToolStripMenuItem2.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NurevoToolStripMenuItem, Me.ABRIRToolStripMenuItem, Me.GuardarComoToolStripMenuItem, Me.SalirToolStripMenuItem})
+        Me.ArchivoToolStripMenuItem2.Image = CType(resources.GetObject("ArchivoToolStripMenuItem2.Image"), System.Drawing.Image)
+        Me.ArchivoToolStripMenuItem2.Name = "ArchivoToolStripMenuItem2"
+        Me.ArchivoToolStripMenuItem2.Size = New System.Drawing.Size(76, 20)
+        Me.ArchivoToolStripMenuItem2.Text = "Archivo"
+        '
+        'NurevoToolStripMenuItem
+        '
+        Me.NurevoToolStripMenuItem.Image = CType(resources.GetObject("NurevoToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.NurevoToolStripMenuItem.Name = "NurevoToolStripMenuItem"
+        Me.NurevoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NurevoToolStripMenuItem.Text = "Nuevo "
+        '
+        'ABRIRToolStripMenuItem
+        '
+        Me.ABRIRToolStripMenuItem.Image = CType(resources.GetObject("ABRIRToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.ABRIRToolStripMenuItem.Name = "ABRIRToolStripMenuItem"
+        Me.ABRIRToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ABRIRToolStripMenuItem.Text = "Abrir"
+        '
+        'GuardarComoToolStripMenuItem
+        '
+        Me.GuardarComoToolStripMenuItem.Image = CType(resources.GetObject("GuardarComoToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.GuardarComoToolStripMenuItem.Name = "GuardarComoToolStripMenuItem"
+        Me.GuardarComoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.GuardarComoToolStripMenuItem.Text = "Guardar como"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Image = CType(resources.GetObject("SalirToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SalirToolStripMenuItem.Text = "Salir"
+        '
+        'EdicionToolStripMenuItem1
+        '
+        Me.EdicionToolStripMenuItem1.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AtrasToolStripMenuItem, Me.AdelanteToolStripMenuItem, Me.CerrarToolStripMenuItem, Me.CopiarToolStripMenuItem, Me.PegarToolStripMenuItem, Me.LimipiarToolStripMenuItem})
+        Me.EdicionToolStripMenuItem1.Image = CType(resources.GetObject("EdicionToolStripMenuItem1.Image"), System.Drawing.Image)
+        Me.EdicionToolStripMenuItem1.Name = "EdicionToolStripMenuItem1"
+        Me.EdicionToolStripMenuItem1.Size = New System.Drawing.Size(74, 20)
+        Me.EdicionToolStripMenuItem1.Text = "Edicion"
+        '
+        'AtrasToolStripMenuItem
+        '
+        Me.AtrasToolStripMenuItem.Image = CType(resources.GetObject("AtrasToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.AtrasToolStripMenuItem.Name = "AtrasToolStripMenuItem"
+        Me.AtrasToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AtrasToolStripMenuItem.Text = "Atras"
+        '
+        'AdelanteToolStripMenuItem
+        '
+        Me.AdelanteToolStripMenuItem.Image = CType(resources.GetObject("AdelanteToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.AdelanteToolStripMenuItem.Name = "AdelanteToolStripMenuItem"
+        Me.AdelanteToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AdelanteToolStripMenuItem.Text = "Adelante"
+        '
+        'CerrarToolStripMenuItem
+        '
+        Me.CerrarToolStripMenuItem.Image = CType(resources.GetObject("CerrarToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.CerrarToolStripMenuItem.Name = "CerrarToolStripMenuItem"
+        Me.CerrarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CerrarToolStripMenuItem.Text = "Cerrar"
+        '
+        'CopiarToolStripMenuItem
+        '
+        Me.CopiarToolStripMenuItem.Image = CType(resources.GetObject("CopiarToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.CopiarToolStripMenuItem.Name = "CopiarToolStripMenuItem"
+        Me.CopiarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CopiarToolStripMenuItem.Text = "Copiar"
+        '
+        'PegarToolStripMenuItem
+        '
+        Me.PegarToolStripMenuItem.Image = CType(resources.GetObject("PegarToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.PegarToolStripMenuItem.Name = "PegarToolStripMenuItem"
+        Me.PegarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.PegarToolStripMenuItem.Text = "Pegar"
+        '
+        'LimipiarToolStripMenuItem
+        '
+        Me.LimipiarToolStripMenuItem.Image = CType(resources.GetObject("LimipiarToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.LimipiarToolStripMenuItem.Name = "LimipiarToolStripMenuItem"
+        Me.LimipiarToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.LimipiarToolStripMenuItem.Text = "Limipiar"
+        '
+        'FormatoToolStripMenuItem
+        '
+        Me.FormatoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FuenteToolStripMenuItem, Me.ColorToolStripMenuItem, Me.NegrillaToolStripMenuItem, Me.CurvaToolStripMenuItem, Me.SubrayadoToolStripMenuItem})
+        Me.FormatoToolStripMenuItem.Image = CType(resources.GetObject("FormatoToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.FormatoToolStripMenuItem.Name = "FormatoToolStripMenuItem"
+        Me.FormatoToolStripMenuItem.Size = New System.Drawing.Size(80, 20)
+        Me.FormatoToolStripMenuItem.Text = "Formato"
+        '
+        'FuenteToolStripMenuItem
+        '
+        Me.FuenteToolStripMenuItem.Image = CType(resources.GetObject("FuenteToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.FuenteToolStripMenuItem.Name = "FuenteToolStripMenuItem"
+        Me.FuenteToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.FuenteToolStripMenuItem.Text = "Fuente"
+        '
+        'ColorToolStripMenuItem
+        '
+        Me.ColorToolStripMenuItem.Image = CType(resources.GetObject("ColorToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.ColorToolStripMenuItem.Name = "ColorToolStripMenuItem"
+        Me.ColorToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.ColorToolStripMenuItem.Text = "Color"
+        '
+        'NegrillaToolStripMenuItem
+        '
+        Me.NegrillaToolStripMenuItem.Image = CType(resources.GetObject("NegrillaToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.NegrillaToolStripMenuItem.Name = "NegrillaToolStripMenuItem"
+        Me.NegrillaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.NegrillaToolStripMenuItem.Text = "Negrilla"
+        '
+        'CurvaToolStripMenuItem
+        '
+        Me.CurvaToolStripMenuItem.Image = CType(resources.GetObject("CurvaToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.CurvaToolStripMenuItem.Name = "CurvaToolStripMenuItem"
+        Me.CurvaToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CurvaToolStripMenuItem.Text = "Curva"
+        '
+        'SubrayadoToolStripMenuItem
+        '
+        Me.SubrayadoToolStripMenuItem.Image = CType(resources.GetObject("SubrayadoToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.SubrayadoToolStripMenuItem.Name = "SubrayadoToolStripMenuItem"
+        Me.SubrayadoToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.SubrayadoToolStripMenuItem.Text = "Subrayado"
+        '
+        'AyudaToolStripMenuItem
+        '
+        Me.AyudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AcercaDeToolStripMenuItem, Me.CrefditosToolStripMenuItem})
+        Me.AyudaToolStripMenuItem.Image = CType(resources.GetObject("AyudaToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
+        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(69, 20)
+        Me.AyudaToolStripMenuItem.Text = "Ayuda"
+        '
+        'AcercaDeToolStripMenuItem
+        '
+        Me.AcercaDeToolStripMenuItem.Image = CType(resources.GetObject("AcercaDeToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.AcercaDeToolStripMenuItem.Name = "AcercaDeToolStripMenuItem"
+        Me.AcercaDeToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.AcercaDeToolStripMenuItem.Text = "Acerca de..."
+        '
+        'CrefditosToolStripMenuItem
+        '
+        Me.CrefditosToolStripMenuItem.Image = CType(resources.GetObject("CrefditosToolStripMenuItem.Image"), System.Drawing.Image)
+        Me.CrefditosToolStripMenuItem.Name = "CrefditosToolStripMenuItem"
+        Me.CrefditosToolStripMenuItem.Size = New System.Drawing.Size(180, 22)
+        Me.CrefditosToolStripMenuItem.Text = "Crefditos"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 27)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(776, 422)
+        Me.RichTextBox1.TabIndex = 3
+        Me.RichTextBox1.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        Me.ContextMenuStrip2.ResumeLayout(False)
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EdicionToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ContextMenuStrip2 As ContextMenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ArchivoToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents NurevoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ABRIRToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents GuardarComoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EdicionToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents AtrasToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AdelanteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CerrarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CopiarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PegarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents LimipiarToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FormatoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FuenteToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ColorToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NegrillaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CurvaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SubrayadoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AyudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AcercaDeToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CrefditosToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,65 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub RichTextBox1_TextChanged(sender As Object, e As EventArgs) Handles RichTextBox1.TextChanged
+
+    End Sub
+
+    Private Sub NurevoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NurevoToolStripMenuItem.Click
+        RichTextBox1.Clear()
+    End Sub
+
+    Private Sub ABRIRToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ABRIRToolStripMenuItem.Click
+        Dim Open As New OpenFileDialog()
+        Dim myStreamReader As System.IO.StreamReader
+        Open.Filter = "Text [*.txt*]|*.txt|All Files [*,*]|*,*"
+        Open.CheckFileExists = True
+        Open.Title = "Abrir Archivo"
+        Open.ShowDialog(Me)
+        Try
+            Open.OpenFile()
+            myStreamReader = System.IO.File.OpenText(Open.FileName)
+            RichTextBox1.Text = myStreamReader.ReadToEnd()
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub LimipiarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LimipiarToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub GuardarComoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles GuardarComoToolStripMenuItem.Click
+        Dim Save As New SaveFileDialog()
+        Dim myStreamWriter As System.IO.StreamWriter
+        Save.Filter = "Text (*.txt)|*.txt|HTML(*.html*)|*.html|PHP(*.php*)|*.php*|All files(*.*)|*.*"
+        Save.CheckPathExists = True
+        Save.Title = "Guardar como"
+        Save.ShowDialog(Me)
+        Try
+            myStreamWriter = System.IO.File.AppendText(Save.FileName)
+            myStreamWriter.Write(RichTextBox1.Text)
+            myStreamWriter.Flush()
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub SalirToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SalirToolStripMenuItem.Click
+        MessageBox.Show("Salir del aplicativo", "Bloc Notas PCQ02", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+        Dim a As Integer
+        a = MessageBox.Show("Salir del aplicativo", "Bloc Notas PCQ02", MessageBoxButtons.OKCancel, MessageBoxIcon.Question)
+        If a = DialogResult.OK Then
+            End
+
+
+        End If
+    End Sub
+
+    Private Sub CrefditosToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CrefditosToolStripMenuItem.Click
+        AboutBox1.Show()
+    End Sub
+End Class
